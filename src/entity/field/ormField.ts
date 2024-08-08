3b88c82d890e6cf23c0218e3678fc9b00705eab0
eveffer <eliveffer@gmail.com>
2024-08-08 07:25:02 +0300

    connected field

--- src/entity/field/ormField.ts
+++ src/entity/field/ormField.ts
@@ -6,19 +6,33 @@ import type {
 export type ExtractFieldKey<T> = T extends EasyField<infer K, infer T> ? K
   : never;
 
+interface Choice {
+  key: string;
+  label: string;
+}
+
+interface FetchedField {
+  key: string;
+  label?: string;
+  description?: string;
+}
+
+interface ConnectedField {
+  entity: string;
+  fetchFields?: Array<FetchedField>;
+}
+
 export interface EasyField<
   P extends PropertyKey = PropertyKey,
   T extends EasyFieldType = EasyFieldType,
 > {
   key: P;
-  label: string;
+  label?: string;
   description?: string;
   required?: boolean;
   readOnly?: boolean;
   fieldType: T;
+  choices?: Choice[];
   defaultValue?: EasyFieldTypeMap[T];
-  connection?: {
-    entity: string;
-    fetchFields?: Array<PropertyKey>;
-  };
+  connection?: ConnectedField;
 }
