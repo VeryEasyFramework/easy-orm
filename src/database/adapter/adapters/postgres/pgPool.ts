48bdb5dffb66adb6ffe4dd624de3164aeeefc144
eveffer <eliveffer@gmail.com>
2024-09-08 06:54:26 +0300

    fix for postgres client locked state

--- src/database/adapter/adapters/postgres/pgPool.ts
+++ src/database/adapter/adapters/postgres/pgPool.ts
@@ -1,97 +1,100 @@
 import {
-  PostgresClient,
+   PostgresClient,
 } from "#/database/adapter/adapters/postgres/pgClient.ts";
 
 import type { PostgresConfig } from "#/database/adapter/adapters/pgAdapter.ts";
 import type {
-  PgClientConfig,
-  QueryResponse,
+   PgClientConfig,
+   QueryResponse,
 } from "#/database/adapter/adapters/postgres/pgTypes.ts";
+import { raiseOrmException } from "#/ormException.ts";
 
 class PostgresPoolClient {
-  locked: boolean;
-  client: PostgresClient;
-  constructor(config: PgClientConfig) {
-    this.locked = false;
-    this.client = new PostgresClient(config);
-  }
-  async connect() {
-    await this.client.connect();
-  }
+   locked: boolean;
+   client: PostgresClient;
+   constructor(config: PgClientConfig) {
+      this.locked = false;
+      this.client = new PostgresClient(config);
+   }
+   async connect() {
+      await this.client.connect();
+   }
 
-  async query<T>(query: string): Promise<QueryResponse<T>> {
-    return await this.client.query<T>(query);
-  }
-  get connected() {
-    return this.client.connected;
-  }
+   async query<T>(query: string): Promise<QueryResponse<T>> {
+      return await this.client.query<T>(query).catch((e) => {
+         this.locked = false;
+
+         throw e;
+      });
+   }
+   get connected() {
+      return this.client.connected;
+   }
 }
 
 export class PostgresPool {
-  private clients: PostgresPoolClient[];
-  private size: number;
-  private lazy: boolean;
-  private clientConfig: PgClientConfig;
-  private maxWait: number;
-  private maxClients: number;
-  constructor(config: PostgresConfig) {
-    this.clients = [];
-    this.maxClients = 10;
-    const options = config.clientOptions;
-    if (options?.unixPath && (options?.host || options?.port)) {
-      throw new Error("Cannot use both unixPath and host/port");
-    }
-    this.clientConfig = options;
-    this.size = config.size || 1;
+   private readonly clients: PostgresPoolClient[];
+   private readonly size: number;
+   private readonly lazy: boolean;
+   private readonly clientConfig: PgClientConfig;
+   private readonly maxWait: number;
+   private readonly maxClients: number;
+   constructor(config: PostgresConfig) {
+      this.clients = [];
+      this.maxClients = 10;
+      const options = config.clientOptions;
+      if (options?.unixPath && (options?.host || options?.port)) {
+         throw new Error("Cannot use both unixPath and host/port");
+      }
+      this.clientConfig = options;
+      this.size = config.size || 1;
 
-    if (this.size > this.maxClients) {
-      this.maxClients = this.size;
-    }
-    this.lazy = config.lazy || false;
-    this.maxWait = 30000;
-    for (let i = 0; i < this.size; i++) {
-      this.clients.push(new PostgresPoolClient(this.clientConfig));
-    }
-  }
+      if (this.size > this.maxClients) {
+         this.maxClients = this.size;
+      }
+      this.lazy = config.lazy || false;
+      this.maxWait = 5000;
+      for (let i = 0; i < this.size; i++) {
+         this.clients.push(new PostgresPoolClient(this.clientConfig));
+      }
+   }
 
-  async initialized() {
-    if (this.lazy) {
-      return;
-    }
-    for (const client of this.clients) {
+   async initialized() {
+      if (this.lazy) {
+         return;
+      }
+      for (const client of this.clients) {
+         await client.connect();
+      }
+   }
+   private async getClient() {
+      let client: PostgresPoolClient | undefined;
+      const start = Date.now();
+      while (!client) {
+         if (Date.now() - start > this.maxWait) {
+            raiseOrmException(
+               "DatabaseError",
+               "Timeout waiting for connection pool client",
+            );
+         }
+         client = this.clients.find((c) => !c.locked);
+         if (!client) {
+            await new Promise((resolve) => setTimeout(resolve, 100));
+         }
+      }
+      client.locked = true;
       await client.connect();
-    }
-  }
-  private async getClient() {
-    let client: PostgresPoolClient | undefined;
-    const start = Date.now();
-    while (!client) {
-      if (Date.now() - start > this.maxWait) {
-        throw new Error("Timeout waiting for client");
-      }
-      client = this.clients.find((c) => !c.locked);
-      if (!client) {
-        await new Promise((resolve) => setTimeout(resolve, 100));
-      }
-    }
-    client.locked = true;
-    await client.connect();
-    return client;
-  }
+      return client;
+   }
 
-  private returnClient(client: PostgresPoolClient) {
-    client.locked = false;
-  }
+   private returnClient(client: PostgresPoolClient) {
+      client.locked = false;
+   }
 
-  async query<T>(query: string): Promise<QueryResponse<T>> {
-    const client = await this.getClient();
-    try {
+   async query<T>(query: string): Promise<QueryResponse<T>> {
+      const client = await this.getClient();
       const result = await client.query<T>(query);
       this.returnClient(client);
       return result;
-    } catch (e) {
-      this.returnClient(client);
-      throw e;
-    }
-  }
+   }
 }
